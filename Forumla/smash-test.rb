8dbd1883d370a41334a8c72177a7448ed254280e
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-01 02:11:18 -0700

    Test

--- Forumla/smash-test.rb
+++ Forumla/smash-test.rb
@@ -1,6 +1,7 @@
 class SmashTest < Formula
   desc "SmAsHeD TesT"
   homepage "https://github.com/smashedr/homebrew-keg"
+
 #  version "0.0.1"
   url "https://github.com/smashedr/homebrew-keg/archive/refs/tags/v0.0.1.zip"
   sha256 "4e2c430796d9ede0ec232bd34545c16508f05827a7340e980d08a5c4dc2bc2f4"
