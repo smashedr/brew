0b01d0e080e2fe8ac69ac7c4b39cdff7fe722d01
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-01 01:46:30 -0700

    Test

--- Forumla/smash-test.rb
+++ Forumla/smash-test.rb
@@ -1,15 +1,15 @@
-class Smashtest < Formula
+class SmashTest < Formula
   desc "SmAsHeD TesT"
-  homepage "https://github.com/smashedr/brew"
+  homepage "https://github.com/smashedr/homebrew-keg"
   url "https://github.com/smashedr/homebrew-keg/archive/refs/tags/v0.0.1.zip"
   version "0.0.1"
   sha256 "4e2c430796d9ede0ec232bd34545c16508f05827a7340e980d08a5c4dc2bc2f4"
 
   def install
-    bin.install "homebrew-keg-0.0.1.zip\homebrew-keg-0.0.1\Formula\smashworld.sh"
+    bin.install "homebrew-keg-0.0.1.zip\homebrew-keg-0.0.1\Formula\smash-test.sh"
   end
 
   test do
-    assert_match "SmAsH WoRlD", shell_output("smashworld.sh")
+    assert_match "SmAsH TesT", shell_output("smash-test.sh")
   end
 end
--- Forumla/smash-test.sh
+++ Forumla/smash-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "SmAsH WoRlD"
+echo "SmAsH TesT"
