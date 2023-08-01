d72dc69c4b1505d42058815ae90f5d1f0dca6bf2
Shane <6071159+smashedr@users.noreply.github.com>
2023-08-01 01:32:00 -0700

    Test

--- Forumla/smashtest.rb
+++ Forumla/smashtest.rb
@@ -3,7 +3,7 @@ class Smashtest < Formula
   homepage "https://github.com/smashedr/brew"
   url "https://github.com/smashedr/homebrew-keg/archive/refs/tags/v0.0.1.zip"
   version "0.0.1"
-  sha256 ""
+  sha256 "4e2c430796d9ede0ec232bd34545c16508f05827a7340e980d08a5c4dc2bc2f4"
 
   def install
     bin.install "homebrew-keg-0.0.1.zip\homebrew-keg-0.0.1\Formula\smashworld.sh"
