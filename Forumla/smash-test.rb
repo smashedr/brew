0010a3b5b244adc15d289a36e7da8d4cf01e1f60
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-31 23:35:23 -0700

    Initial Commit

---
+++ Forumla/smash-test.rb
@@ -0,0 +1,15 @@
+class SmashTest < Formula
+  desc "SmAsHeD TesT"
+  homepage "https://github.com/smashedr/brew"
+  url "https://raw.githubusercontent.com/smashedr/homebrew-test/master/Formula/smash-world.sh"
+  version "0.0.1"
+  sha256 ""
+
+  def install
+    bin.install "smash-world.sh"
+  end
+
+  test do
+    assert_match "SmAsH WoRlD", shell_output("smash-world.sh")
+  end
+end
