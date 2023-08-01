2f2e573301622c07f6e12917fa1a013b543232a7
Shane <6071159+smashedr@users.noreply.github.com>
2023-07-31 23:40:01 -0700

    Test

--- Forumla/smash-test.rb
+++ Forumla/smash-test.rb
@@ -1,7 +1,7 @@
 class SmashTest < Formula
   desc "SmAsHeD TesT"
   homepage "https://github.com/smashedr/brew"
-  url "https://raw.githubusercontent.com/smashedr/homebrew-test/master/Formula/smash-world.sh"
+  url "https://raw.githubusercontent.com/smashedr/brew/master/Forumla/smash-world.sh"
   version "0.0.1"
   sha256 ""
 
